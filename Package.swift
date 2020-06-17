29eb70156c64e859784581bc7e856a57b48136ef
Andrew Barba <abarba.77@gmail.com>
2020-06-17 18:27:50 -0400

    Support Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "Rudder",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(name: "Rudder", targets: ["Rudder"])
+    ],
+    targets: [
+        .target(
+            name: "Rudder",
+            path: "Rudder",
+            cSettings: [
+                .headerSearchPath("Classes/**")
+            ]
+        )
+    ]
+)
