08d80ce981b59abd9ae90e3821d30f0c2dde7f9f
Arnab <arnab.compsc@gmail.com>
2020-08-05 19:21:05 +0530

    Bumpeed version to 1.0.3-patch.3

--- Rudder/Classes/RSConstants.m
+++ Rudder/Classes/RSConstants.m
@@ -17,5 +17,5 @@ int const RSSleepTimeout = 10;
 NSString *const RSControlPlaneUrl = @"https://api.rudderlabs.com";
 bool const RSTrackLifeCycleEvents = YES;
 bool const RSRecordScreenViews = NO;
-NSString *const RS_VERSION = @"1.0.3-patch.2";
+NSString *const RS_VERSION = @"1.0.3-patch.3";
 @end
