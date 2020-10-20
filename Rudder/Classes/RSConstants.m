a2fb4d97886a6c409389b9d95c66c58ad117bc91
Arnab <arnab.compsc@gmail.com>
2020-10-20 22:23:06 +0530

    Bumped version

--- Rudder/Classes/RSConstants.m
+++ Rudder/Classes/RSConstants.m
@@ -17,5 +17,5 @@ int const RSSleepTimeout = 10;
 NSString *const RSControlPlaneUrl = @"https://api.rudderlabs.com";
 bool const RSTrackLifeCycleEvents = YES;
 bool const RSRecordScreenViews = NO;
-NSString *const RS_VERSION = @"1.0.8";
+NSString *const RS_VERSION = @"1.0.9";
 @end
