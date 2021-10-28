487f2b1b9e0a7fa3b5fa417a7cd7a7d60c5a3c4a
Pallab Maiti <pallab@rudderstack.com>
2021-10-28 09:38:00 +0530

    Remove Pods

--- Pods/Target Support Files/Rudder-iOS/Rudder-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Rudder_iOS : NSObject
-@end
-@implementation PodsDummy_Rudder_iOS
-@end
--- Pods/Target Support Files/Rudder-tvOS/Rudder-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Rudder_tvOS : NSObject
-@end
-@implementation PodsDummy_Rudder_tvOS
-@end
