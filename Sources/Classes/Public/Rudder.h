de621ed35a9f08533ce1d11006cb63f459fc9a00
Pallab Maiti <pallab@rudderstack.com>
2021-11-02 12:11:16 +0530

    Project settings changed

--- Sources/Classes/Public/Rudder.h
+++ Sources/Classes/Public/Rudder.h
@@ -15,45 +15,6 @@
 
 #import "RSLogger.h"
 
-// Ecommerce Section
-#import "RSECommerceProductBuilder.h"
-#import "RSECommerceFilterBuilder.h"
-#import "RSECommerceSortBuilder.h"
-#import "RSECommercePromotionBuilder.h"
-#import "RSECommerceOrderBuilder.h"
-#import "RSECommerceCheckoutBuilder.h"
-#import "RSECommerceCouponBuilder.h"
-#import "RSECommerceWishListBuilder.h"
-
-#import "RSProductSearchedEvent.h"
-#import "RSProductListViewedEvent.h"
-#import "RSProductListFilteredEvent.h"
-#import "RSPromotionViewedEvent.h"
-#import "RSPromotionClickedEvent.h"
-#import "RSProductClickedEvent.h"
-#import "RSProductViewedEvent.h"
-#import "RSProductAddedToCartEvent.h"
-#import "RSProductRemovedEvent.h"
-#import "RSCartViewedEvent.h"
-#import "RSCheckoutStartedEvent.h"
-#import "RSCheckoutStepViewedEvent.h"
-#import "RSCheckoutStepCompletedEvent.h"
-#import "RSPaymentInfoEnteredEvent.h"
-#import "RSOrderUpdatedEvent.h"
-#import "RSOrderCompletedEvent.h"
-#import "RSOrderRefundedEvent.h"
-#import "RSOrderCancelledEvent.h"
-#import "RSCouponEnteredEvent.h"
-#import "RSCouponAppliedEvent.h"
-#import "RSCouponDeniedEvent.h"
-#import "RSCouponRemovedEvent.h"
-#import "RSProductAddedToWishListEvent.h"
-#import "RSProductRemovedFromWishListEvent.h"
-#import "RSWishListProductAddedToCartEvent.h"
-#import "RSProductSharedEvent.h"
-#import "RSCartSharedEvent.h"
-#import "RSProductReviewedEvent.h"
-
 //! Project version number for RS.
 FOUNDATION_EXPORT double RSVersionNumber;
 
