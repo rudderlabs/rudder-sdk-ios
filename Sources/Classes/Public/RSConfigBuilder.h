eba3a166b4e812b41c124eb151988fb4ac2d1a86
Desu Sai Venkat <saivenkatdesu@gmail.com>
2021-12-30 12:14:29 +0530

    Fixed WithEnableBackGroundMode naming in RSConfigBuilder.h

--- Sources/Classes/Public/RSConfigBuilder.h
+++ Sources/Classes/Public/RSConfigBuilder.h
@@ -29,7 +29,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (instancetype) withConfigRefreshInteval: (int) configRefreshInterval;
 - (instancetype) withTrackLifecycleEvens: (BOOL) trackLifecycleEvents;
 - (instancetype) withRecordScreenViews: (BOOL) recordScreenViews;
-- (instancetype) withBackgroundMode:(BOOL) enableBackgroundMode;
+- (instancetype) withEnableBackgroundMode:(BOOL) enableBackgroundMode;
 - (instancetype) withConfigPlaneUrl: (NSString*) configPlaneUrl __attribute((deprecated("Use withControlPlaneUrl instead.")));
 - (instancetype) withControlPlaneUrl: (NSString*) controlPlaneUrl;
 - (instancetype) withControlPlaneURL: (NSURL*) controlPlaneURL;
