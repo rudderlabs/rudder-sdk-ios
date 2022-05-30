e05b0a918edb11da54cb80b3aa220dc176b0b22a
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2022-05-30 18:36:14 +0530

    Optimised code: Now empty properties will be filtered.

--- Sources/Classes/Helpers/Platforms/iOS/RSiOSLifecycleEvents.swift
+++ Sources/Classes/Helpers/Platforms/iOS/RSiOSLifecycleEvents.swift
@@ -15,7 +15,7 @@ import UIKit
 class RSiOSLifecycleEvents: RSPlatformPlugin, RSiOSLifecycle {
     let type = PluginType.before
     var client: RSClient?
-    var isItLaunchedFirstTime: Bool = true
+    var isFirstTimeLaunch: Bool = true
     
     @RSAtomic private var didFinishLaunching = false
     
@@ -33,26 +33,25 @@ class RSiOSLifecycleEvents: RSPlatformPlugin, RSiOSLifecycle {
         let currentBuild = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
         
         if previousVersion == nil {
-            client?.track("Application Installed", properties: [
-                "version": currentVersion ?? "",
-                "build": currentBuild ?? ""
-            ])
+            client?.track("Application Installed", properties: getApplicationInstalledProps(
+                currentVersion: currentVersion,
+                currentBuild: currentBuild
+            ))
         } else if currentVersion != previousVersion {
-            client?.track("Application Updated", properties: [
-                "previous_version": previousVersion ?? "",
-                "previous_build": previousBuild ?? "",
-                "version": currentVersion ?? "",
-                "build": currentBuild ?? ""
-            ])
+            client?.track("Application Updated", properties: getApplicationUpdatedProps(
+                previousVersion: previousVersion,
+                previousBuild: previousBuild,
+                currentVersion: currentVersion,
+                currentBuild: currentBuild
+            ))
         }
         
-        client?.track("Application Opened", properties: [
-            "from_background": false,
-            "version": currentVersion ?? "",
-            "build": currentBuild ?? "",
-            "referring_application": launchOptions?[UIApplication.LaunchOptionsKey.sourceApplication] ?? "",
-            "url": launchOptions?[UIApplication.LaunchOptionsKey.url] ?? ""
-        ])
+        client?.track("Application Opened", properties: getApplicationOpenedProps(
+            currentVersion: currentVersion,
+            currentBuild: currentBuild,
+            referring_application: launchOptions?[UIApplication.LaunchOptionsKey.sourceApplication],
+            url: launchOptions?[UIApplication.LaunchOptionsKey.url]
+        ))
         
         RSUserDefaults.saveApplicationVersion(currentVersion)
         RSUserDefaults.saveApplicationBuild(currentBuild)
@@ -63,20 +62,19 @@ class RSiOSLifecycleEvents: RSPlatformPlugin, RSiOSLifecycle {
             return
         }
         
-        // If it is launched first time then we shouldn't be sending Application Opened event again.
-        if self.isItLaunchedFirstTime == true {
-            isItLaunchedFirstTime = false
+        // If app is launched first time then the Application Opened event will not be sent again.
+        if self.isFirstTimeLaunch == true {
+            self.isFirstTimeLaunch = false
             return
         }
         
         let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
         let currentBuild = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
         
-        client?.track("Application Opened", properties: [
-            "from_background": true,
-            "version": currentVersion ?? "",
-            "build": currentBuild ?? ""
-        ])
+        client?.track("Application Opened", properties: getApplicationOpenedProps(
+            currentVersion: currentVersion,
+            currentBuild: currentBuild
+        ))
     }
     
     func applicationDidEnterBackground(application: UIApplication?) {
@@ -95,6 +93,66 @@ class RSiOSLifecycleEvents: RSPlatformPlugin, RSiOSLifecycle {
         if didFinishLaunching == false {
             self.application(nil, didFinishLaunchingWithOptions: nil)
         }
+    }
+}
+
+extension RSiOSLifecycleEvents {
+    func getApplicationUpdatedProps(previousVersion: String?, previousBuild: String?, currentVersion: String?, currentBuild: String?) -> [String: Any] {
+        var properties = [String: Any]()
+        if let previousVersion = previousVersion, !previousVersion.isEmpty {
+            properties["previous_version"] = previousVersion
+        }
+        if let previousBuild = previousBuild, !previousBuild.isEmpty {
+            properties["previous_build"] = previousBuild
+        }
+        if let currentVersion = currentVersion, !currentVersion.isEmpty {
+            properties["version"] = currentVersion
+        }
+        if let currentBuild = currentBuild, !currentBuild.isEmpty {
+            properties["build"] = currentBuild
+        }
+        return properties
+    }
+    
+    func getApplicationInstalledProps(currentVersion: String?, currentBuild: String?) -> [String: Any]{
+        var properties = [String: Any]()
+        if let currentVersion = currentVersion, !currentVersion.isEmpty {
+            properties["version"] = currentVersion
+        }
+        if let currentBuild = currentBuild, !currentBuild.isEmpty {
+            properties["build"] = currentBuild
+        }
+        return properties
+    }
+    
+    func getApplicationOpenedProps(currentVersion: String?, currentBuild: String?, referring_application: Any?, url: Any?) -> [String: Any] {
+        var properties = [String: Any]()
+        properties["from_background"] = false
+        if let currentVersion = currentVersion, !currentVersion.isEmpty {
+            properties["version"] = currentVersion
+        }
+        if let currentBuild = currentBuild, !currentBuild.isEmpty {
+            properties["build"] = currentBuild
+        }
+        if let referring_application = referring_application {
+            properties["referring_application"] = referring_application
+        }
+        if let url = url {
+            properties["url"] = url
+        }
+        return properties
+    }
+    
+    func getApplicationOpenedProps(currentVersion: String?, currentBuild: String?) -> [String: Any] {
+        var properties = [String: Any]()
+        properties["from_background"] = true
+        if let currentVersion = currentVersion, !currentVersion.isEmpty {
+            properties["version"] = currentVersion
+        }
+        if let currentBuild = currentBuild, !currentBuild.isEmpty {
+            properties["build"] = currentBuild
+        }
+        return properties
     }
 }
 
