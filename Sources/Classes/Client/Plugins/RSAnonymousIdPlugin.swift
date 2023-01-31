60a0d7042cf9a208f9dea1c2c6f677f335891cef
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2023-01-31 21:56:34 +0530

    fix: enhance support for anonymousId for all supported platforms (#257)
    * fix: enhance support for anonymousId for all supported platforms
    
    * chore: remove mistakenly added code

--- Sources/Classes/Client/Plugins/RSAnonymousIdPlugin.swift
+++ Sources/Classes/Client/Plugins/RSAnonymousIdPlugin.swift
@@ -8,13 +8,85 @@
 
 import Foundation
 
+// MARK: - iOS, tvOS, Catalyst
+
+#if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
+import SystemConfiguration
+import UIKit
+#if !os(tvOS)
+import WebKit
+#endif
+#endif
+
+// MARK: - watchOS
+
+#if os(watchOS)
+import WatchKit
+import Network
+#endif
+
+// MARK: - macOS
+
+#if os(macOS)
+import Cocoa
+import WebKit
+#endif
+
+
 class RSAnonymousIdPlugin: RSPlatformPlugin {
     let type = PluginType.before
     var client: RSClient?
     
-    var anonymousId = UUID().uuidString.lowercased()
+    var anonymousId: String?
 
-    required init() { }
+    required init() {
+#if os(iOS) || os(tvOS)
+        anonymousId = UIDevice.current.identifierForVendor?.uuidString.lowercased()
+#endif
+#if os(watchOS)
+        anonymousId = WKInterfaceDevice.current().identifierForVendor?.uuidString.lowercased()
+#endif
+#if os(macOS)
+        anonymousId = macAddress(bsd: "en0")
+#endif
+    }
+    
+    private func macAddress(bsd: String) -> String? {
+        let MAC_ADDRESS_LENGTH = 6
+        let separator = ":"
+        
+        var length: size_t = 0
+        var buffer: [CChar]
+        
+        let bsdIndex = Int32(if_nametoindex(bsd))
+        if bsdIndex == 0 {
+            return nil
+        }
+        let bsdData = Data(bsd.utf8)
+        var managementInfoBase = [CTL_NET, AF_ROUTE, 0, AF_LINK, NET_RT_IFLIST, bsdIndex]
+        
+        if sysctl(&managementInfoBase, 6, nil, &length, nil, 0) < 0 {
+            return nil
+        }
+        
+        buffer = [CChar](unsafeUninitializedCapacity: length, initializingWith: {buffer, initializedCount in
+            for x in 0..<length { buffer[x] = 0 }
+            initializedCount = length
+        })
+        
+        if sysctl(&managementInfoBase, 6, &buffer, &length, nil, 0) < 0 {
+            return nil
+        }
+        
+        let infoData = Data(bytes: buffer, count: length)
+        let indexAfterMsghdr = MemoryLayout<if_msghdr>.stride + 1
+        let rangeOfToken = infoData[indexAfterMsghdr...].range(of: bsdData)!
+        let lower = rangeOfToken.upperBound
+        let upper = lower + MAC_ADDRESS_LENGTH
+        let macAddressData = infoData[lower..<upper]
+        let addressBytes = macAddressData.map { String(format: "%02x", $0) }
+        return addressBytes.joined(separator: separator)
+    }
     
     func execute<T: RSMessage>(message: T?) -> T? {
         guard var workingMessage = message else { return message }
