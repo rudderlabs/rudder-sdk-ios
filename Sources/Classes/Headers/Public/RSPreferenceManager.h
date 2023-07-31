307e7304cb9f385dcdc77943e05ee75c79c66006
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2023-07-31 14:01:01 +0530

    fix: replay message queue dumping logic  (#348)
    * fix: replay message queue dumping logic
    
    * revert: previous logic to mark events as device mode processing done on App Update
    
    * test: add new db unit test case and fix old one

--- Sources/Classes/Headers/Public/RSPreferenceManager.h
+++ Sources/Classes/Headers/Public/RSPreferenceManager.h
@@ -79,9 +79,6 @@ extern NSString *const RSSessionAutoTrackStatus;
 - (void) saveAutoTrackingStatus: (BOOL) autoTrackingStatus;
 - (BOOL) getAutoTrackingStatus;
 
-- (void) saveEventDeletionStatus;
-- (BOOL) getEventDeletionStatus;
-
 @end
 
 NS_ASSUME_NONNULL_END
