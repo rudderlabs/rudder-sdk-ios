d478c23de1e671d2d112ab1ab8181d55064ed427
Pallab Maiti <pallab@rudderstack.com>
2023-08-24 12:08:45 +0530

    fix: spm build is failing for missing dependency (#373)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Rudder",
     platforms: [
-        .iOS(.v9), .tvOS(.v10)
+        .iOS(.v12), .tvOS(.v11), .watchOS("7.0")
     ],
     products: [
         .library(
@@ -13,19 +13,26 @@ let package = Package(
             targets: ["Rudder"]
         )
     ],
+    dependencies: [
+        .package(name: "MetricsReporter", url: "https://github.com/rudderlabs/metrics-reporter-ios", from: "1.0.0"),
+    ],
     targets: [
         .target(
             name: "Rudder",
+            dependencies: [
+                .product(name: "MetricsReporter", package: "MetricsReporter"),
+            ],
             path: "Sources",
             sources: ["Classes/"],
             publicHeadersPath: "Classes/Headers/Public/",
             cSettings: [
-                .headerSearchPath("Classes/Headers/")
+                .headerSearchPath("Classes/Headers/"),
+                .unsafeFlags(["-DSQLITE_HAS_CODEC", "-DSQLITE_TEMP_STORE=3", "-DSQLCIPHER_CRYPTO_CC", "-DNDEBUG"])
             ]
         ),
         .testTarget(
             name: "RudderTests",
-            dependencies: ["Rudder"],
+            dependencies: ["Rudder", "MetricsReporter"],
             path: "Tests"
         ),
     ]
